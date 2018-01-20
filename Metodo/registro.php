0506f07b45395cde947ffc7a0f77fedfec4f265e
KVanessaT <31851724+KVanessaT@users.noreply.github.com>
2018-01-20 00:40:31 -0500

    registro.php

--- Metodo/registro.php
+++ Metodo/registro.php
@@ -9,11 +9,11 @@ $fullnameCliente= $_POST['clien-fullname'];
 $apeCliente= $_POST['clien-lastname'];
 $passCliente=$_POST['clien-pass'];
 //hasta aqui ....
-if(!$nitCliente=="" && !$nameCliente=="" && !$apeCliente=="" && !$dirCliente=="" && !$phoneCliente=="" && !$emailCliente=="" && !$fullnameCliente==""){
+if(!$nitCliente=="" && !$nameCliente=="" && !$apeCliente=="" && !$dirCliente==""&& !$fullnameCliente==""){
     $verificar=  ejecutarSQL::consultar("select * from Usuarios where idUsuario='".$nitCliente."'");
     $verificaltotal = mysql_num_rows($verificar);
     if($verificaltotal<=0){
-        if(consultasSQL::InsertSQL("Usuarios", "idUsuarios, nombreUsuario, apellidoUsuario, edadUsuario, emailUsuario", "'$nitCliente','$nameCliente','$fullnameCliente','$apeCliente','$dirCliente', '$passCliente','$phoneCliente','$emailCliente'")){
+        if(consultasSQL::InsertSQL("Usuarios", "idUsuarios, nombreUsuario, apellidoUsuario, edadUsuario, emailUsuario", "'$nitCliente','$nameCliente','$fullnameCliente','$apeCliente', '$passCliente'")){
             echo '<br>El registro se completo con éxito';
         }else{
            echo '<br>Ha ocurrido un error.<br>Por favor intente nuevamente'; 
